7c9738a10636fc90f23339b672f4cec9058753fb
Elijah Apreko <elijahapreko@gmail.com>
2024-08-12 10:35:28 +0000

    update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from './config/config.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { GitHubModule } from './github/github.module';
 import { AppService } from './app.service';
 import { AuthService } from './auth/auth.service';
-import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule,
+    MongooseModule.forRoot(process.env.MONGODB_URI), // Connect to MongoDB
+    AuthModule,
+    UsersModule,
+    GitHubModule,
+  ],
   controllers: [AppController],
-  providers: [AppService, AuthService],
+  providers: [AppService],
 })
 export class AppModule {}
